c93335da9a4702d6eb470babd00df55a1fd1e900
LeoKitabdjian <leo.kitabdjian06@gmail.com>
2023-04-12 00:45:35 +0200

    confetti sound effect

--- Assets/Confettis/ConfettiManager.cs
+++ Assets/Confettis/ConfettiManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ConfettiManager : MonoBehaviour
 {
     public ParticleSystem particleSystem;
+    public AudioSource conffetiSound;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,7 @@ public class ConfettiManager : MonoBehaviour
 
     public void PlayConfetti()
     {
+        conffetiSound.Play();
         particleSystem.Play(true);
     }
 }
