85223ed77280d6d53bbb70fa5f8e06f6f58d852e
LeoKitabdjian <leo.kitabdjian06@gmail.com>
2023-04-11 11:34:14 +0200

    credits

--- Assets/Interactables/ConfettiButton.cs
+++ Assets/Interactables/ConfettiButton.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ConfettiButton : MonoBehaviour, IInteractable
 {
@@ -9,11 +10,41 @@ public class ConfettiButton : MonoBehaviour, IInteractable
 
     public ConfettiManager confetti;
 
+    public Image blackScreen;
+
+    private bool hasStarted = false;
+
+    public float fadeSpeed;
+
+    void Start()
+    {
+        StartCoroutine(FadeToBlack());
+    }
+
     public bool Interact(Interactor interactor)
     {
         confetti.PlayConfetti();
+        if (!hasStarted)
+        {
+            StartCoroutine(FadeToBlack());
+            hasStarted = true;
+        }
         //Destroy(this);
         return true;
+    }
+
+    private IEnumerator FadeToBlack()
+    {
+        Color objectColor = blackScreen.color;
+        float fadeAmount;
+        while (blackScreen.color.a < 1) 
+        {
+            fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            blackScreen.color = objectColor;
+            yield return null;
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void ShowAction()
